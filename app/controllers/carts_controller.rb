7566175f804ba38ec6ac1aefabd223ba8a769786
Polly Sileo <pollysileo@gmail.com>
2017-04-13 07:50:29 -0400

    Better styling for hiding an empty cart

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -29,7 +29,7 @@ class CartsController < ApplicationController
 
     respond_to do |format|
       if @cart.save
-        format.html { redirect_to @cart, notice: 'Cart was successfully created.' }
+        format.html { redirect_to store_index_url }
         format.json { render :show, status: :created, location: @cart }
       else
         format.html { render :new }
